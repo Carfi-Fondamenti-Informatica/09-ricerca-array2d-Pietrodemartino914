7ee1e84be4bc8eab175a0b9452a4601fe6b2f452
Pietrodemartino914 <115526682+Pietrodemartino914@users.noreply.github.com>
2022-12-13 12:01:14 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,15 +1,14 @@
-
-
 #include "lib.h"
 
-int confronto (char a [10][20],char b [10][20]){
+int confronto (char a [10][20],char b [1][20]){
     for (int i = 0; i < 10; i++) {
         if (a[i][0]==b[0][0]){
             for (int j = 0; j < 20; j++) {
-                if(a[i][j]==b[0][j]) {
-                    return ++i;
-                }else{
+                if(a[i][j]!=b[0][j]) {
                     break;
+                }
+                if (j==19 and a[i][j]==b[0][j]){
+                    return ++i;
                 }
             }
         }
