1543687d6028ff084000a7eb416f3cce8dbed95d
Pietrodemartino914 <115526682+Pietrodemartino914@users.noreply.github.com>
2022-12-13 11:36:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+int main() {
+    char lista [10][20];
+    char nome [1][20];
+    for (int i ; i<10;i++){     //spaziatura array bidimensionale
+        for (int j = 0; j < 20; j++) {
+            lista[i][j]=' ';
+        }
+    }
+    for (int i = 0; i < 10; i++) { //inserimento array bidimensionale
+        cin >> lista [i];
+    }
+
+    for (int i = 0; i < 20; i++) {  //spaziatura array monodimensionale
+        nome[0][i]=' ';
+    }
+    cin>> nome[0]; //inserimento array monodimensionale
+
+
+
+
+    if ( confronto(lista,nome)!=-1){
+        cout<<confronto(lista,nome);
+    }else {
+        cout<< "non presente";
+    }
+
+    return 0;
 }
